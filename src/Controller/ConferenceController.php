48255a69c2dd33cedc2b61d40c4037fabde36741
romano brentani <romano.brentani@unibe.ch>
2022-05-16 11:06:38 +0200

    created entity classes and tables in app_database:
    -Comment
    -Conference

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,9 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference", name="conference")
      */
-    public function index(): Response
+    public function index(Request  $request): Response
     {
+
         return $this->render('conference/index.html.twig', [
             'controller_name' => 'ConferenceController',
         ]);
---
+++ src/Controller/PersonController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonController extends AbstractController
+{
+    /**
+     * @Route("/person", name="app_person")
+     */
+    public function index(): Response
+    {
+        $name = "Romano";
+        return $this->render('person/index.html.twig', [
+            'controller_name' => 'PersonController',
+            'lastName' => $name
+        ]);
+    }
+}
